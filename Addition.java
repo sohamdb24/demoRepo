7deafddb63f70d33a4502786ff17b82e5463615d
sohamdb24 <sohambugad@gmail.com>
2024-07-31 08:51:18 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the first number
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        // Prompt the user to enter the second number
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        // Add the numbers
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Close the scanner
+        scanner.close();
+    }
+}
